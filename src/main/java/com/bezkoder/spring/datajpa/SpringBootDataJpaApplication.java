aac9a43bb8649c03d4f7a702a761cb1f19d86ae4
subhanicool01 <subhanicool01@gmail.com>
2024-10-14 16:03:52 +0530

    Update SpringBootDataJpaApplication.java

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,6 +2,7 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringBootDataJpaApplication {
