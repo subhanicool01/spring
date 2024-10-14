9d6679a4b2e997ccfe044cc51a2cd961445a76c3
Mehaboob Shaik <mshaik@procurant.com>
2024-10-14 16:15:18 +0530

    added annotations

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -3,7 +3,12 @@ package com.bezkoder.spring.datajpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan({"com.bezkoder.spring.datajpa"})
+@EntityScan("com.bezkoder.spring.datajpa.model")
+@EnableJpaRepositories("com.bezkoder.spring.datajpa.repository")
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringBootDataJpaApplication {
 
--- src/main/java/com/bezkoder/spring/datajpa/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/datajpa/repository/TutorialRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bezkoder.spring.datajpa.model.Tutorial;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 	List<Tutorial> findByPublished(boolean published);
 	List<Tutorial> findByTitleContaining(String title);
